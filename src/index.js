dc841ff2007651528bd651238618d696b8c07228
tatialveso <tatiialveso@gmail.com>
2020-08-01 12:00:37 -0300

    create error page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter, Switch, Route, Redirect,
+  BrowserRouter, Switch, Route,
 } from 'react-router-dom';
 import './reset.css';
+import styled from 'styled-components';
 import Home from './pages/Home';
 import CadastroVideo from './pages/Cadastro/Video';
 import CadastroCategoria from './pages/Cadastro/Categoria';
-import ErrorPage from './pages/404';
+import PageDefault from './components/PageDefault';
+
+const ErrorHeader = styled.h1`
+  text-align: center;
+  font-size: 200px;
+  font-weight: bold;
+  margin: 0;
+  color: var(--primary);
+`;
+
+const ErrorMessage = styled.p`
+  text-align: center;
+  font-size: 30px;
+`;
+
+const ErrorPage = () => (
+  <PageDefault>
+    <ErrorHeader>404</ErrorHeader>
+    <ErrorMessage>Ops... A página que você procura não existe. Tente novamente.</ErrorMessage>
+  </PageDefault>
+);
 
 ReactDOM.render(
   <BrowserRouter>
@@ -15,8 +36,7 @@ ReactDOM.render(
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
-      <Redirect to="/404" />
-      <Route path="*" component={ErrorPage} />
+      <Route component={ErrorPage} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root'),
--- src/pages/404/style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Title = styled.h1`
-    color: var(--white);
-    background-color: var(--black);
-`;
